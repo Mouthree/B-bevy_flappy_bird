fbf35aca6a833ada9946c906da135392a7490468
Mouthree <mouthree@qq.com>
2026-05-16 23:22:58 +0800

    实现了管道的刷新以及移动

--- src/entity/pipes.rs
+++ src/entity/pipes.rs
@@ -1,10 +1,12 @@
-use bevy::{app::{App, Plugin}, asset::{AssetServer, Handle}, camera::visibility::Visibility, ecs::{children, component::Component, system::{Commands, Res}}, image::{self, Image}, sprite::{BorderRect, SliceScaleMode, Sprite, SpriteImageMode, TextureSlicer}, time::Time, transform::components::Transform, utils::default};
+//!管道
+use std::time::Duration;
 
-use crate::constants::{CANVAS_SIZE, GAP_SIZE, PIPE_SIZE};
+use bevy::{app::{App, FixedUpdate, Plugin}, asset::{AssetServer, Handle}, camera::visibility::Visibility, color::Color, ecs::{children, component::Component, query::With, schedule::IntoScheduleConfigs, system::{Commands, Query, Res}}, image::{self, Image}, math::Vec2, sprite::{BorderRect, SliceScaleMode, Sprite, SpriteImageMode, TextureSlicer}, time::{Fixed, Time, common_conditions::on_timer}, transform::components::Transform, utils::default};
+use crate::{constants::{CANVAS_SIZE, GAP_SIZE, PIPE_SIZE, PIPE_SPEED}, entity::pipes};
 
 ///整个管道
 #[derive(Component)]
-pub struct Pipe;
+pub struct PipeAll;
 
 ///管道顶上
 #[derive(Component)]
@@ -14,32 +16,46 @@ pub struct PipeTop;
 #[derive(Component)]
 pub struct PipeBotton;
 
-pub struct PipPlugin;
-impl Plugin for PipPlugin {
+///管道中间
+#[derive(Component)]
+pub struct PointsGate;
+
+///管道插件
+pub struct Pipe;
+impl Plugin for Pipe {
     fn build(&self, app: &mut App) {
-        
+        app.add_systems(FixedUpdate, (
+           pip_start_up.run_if(on_timer(Duration::from_millis(1000))),
+           pipe_move
+        ));
     }
 }
 
 ///创建随机高度的管道
 fn pip_start_up(mut commands: Commands, asset_server: Res<AssetServer>, time: Res<Time>) {
     let image: Handle<Image> = asset_server.load("pipe.png");
+    //设置图片显示模式为九宫格
     let image_mode = SpriteImageMode::Sliced(
+        //具体切割逻辑
         TextureSlicer {
+            //左右7px, 上下10px不变
             border: BorderRect::axes(7., 10.),
+            //其余直接拉伸
             center_scale_mode: SliceScaleMode::Stretch,
             ..default()
         }
     );
-
+    //新生成管道位置
     let transform = Transform::from_xyz(CANVAS_SIZE.x, 0., 1.);
-    let pip_offset = PIPE_SIZE.y / 2. + GAP_SIZE / 2.;
+    //移动管道, 布局为管道下-空隙-管道上
+    let pipe_offset = PIPE_SIZE.y / 2. + GAP_SIZE / 2.;
+    //随机的移动
     let gap_y_position = (time.elapsed_secs() * 4.2309875).sin() * CANVAS_SIZE.y / 4.;
-
+    //创建管道实体, 由上管, 空隙, 下管组成
     commands.spawn((
         transform,
         Visibility::Visible,
-        Pipe,
+        PipeAll,
         children![(
             Sprite {
                 image: image.clone(),
@@ -47,14 +63,39 @@ fn pip_start_up(mut commands: Commands, asset_server: Res<AssetServer>, time: Re
                 image_mode: image_mode.clone(),
                 ..default()
             },
-            Transform::from_xyz(0., pip_offset + gap_y_position, 1.),
+            Transform::from_xyz(0., pipe_offset + gap_y_position, 1.),
             PipeTop
         ),
         (
-            
+            //设置为不可见
+            Visibility::Hidden,
+            Sprite {
+                //这个色在调试的时候可能会用到
+                color: Color::WHITE,
+                custom_size: Some(Vec2::new(10., GAP_SIZE)),
+                ..default()
+            },
+            Transform::from_xyz(0., gap_y_position, 1.),
+            PointsGate
         ),
         (
-            
+            Sprite {
+                image,
+                custom_size: Some(PIPE_SIZE),
+                image_mode,
+                ..default()
+            },
+            Transform::from_xyz(0., -pipe_offset + gap_y_position, 1.),
+            PipeBotton
         )]
     ));
 }
+
+///设置管道移动
+fn pipe_move(mut pipes: Query<&mut Transform, With<PipeAll>>, time: Res<Time>) {
+    for mut pipe in &mut pipes {
+        pipe.translation.x -= PIPE_SPEED * time.delta_secs();
+        pipe.translation.x = pipe.translation.x.round();
+    }
+}
+
